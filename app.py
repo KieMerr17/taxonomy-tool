339984ded8cc8b700f5b1f22b80cab11874130ed
Kieran Merrett <116359190+KieMerr17@users.noreply.github.com>
2024-06-19 20:35:55 +0100

    add multi page function

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 import streamlit as st
+from taxonomy import slow, speed
 
-st.title('Hello')
+# Sidebar
+st.sidebar.title("Taxonomy WHATS")
+
+# Dictionary to link page names to Taxonomy
+taxonomy = {
+    "Slow": slow,
+    "Speed": speed,
+}
+
+# Sidebar navigation
+selection = st.sidebar.radio("Go to", list(taxonomy.keys()))
+
+# Run the selected page
+pages = taxonomy[selection]
+pages.app()
+
