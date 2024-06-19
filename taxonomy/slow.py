35886363a5ab2886fbb66edeeb21bd0a03e788e2
Kieran Merrett <116359190+KieMerr17@users.noreply.github.com>
2024-06-19 21:54:39 +0100

    add select box to slow page

--- taxonomy/slow.py
+++ taxonomy/slow.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
 def app():
-    st.title("Slow")
+    st.title("Taxonomy: 'SPEED'")
+
+    st.selectbox(
+        'Please select which "WHY" examples you would like to see:',
+            ('Amber Light', 
+             'Stationary Vehicle'
+             ))
+
