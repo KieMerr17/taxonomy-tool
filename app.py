4650c95324a9c7ecbf15f68a25c04148120234b7
Kieran Merrett <116359190+KieMerr17@users.noreply.github.com>
2024-06-21 09:36:55 +0100

    add all taxonomy 'WHATS' to project with x1 folder in each

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import streamlit as st
-from taxonomy import accelerate, early_turn
+from taxonomy import accelerate, early_turn, close_proximity, diversion, emergency_services, lane_position, late_turn, overtake, route, slow, speed, stopped, uncategorised
 
 # Sidebar
 st.sidebar.title("Taxonomy WHATS")
 
 # Dictionary to link page names to Taxonomy
 taxonomy = {
-    "Early Turn": early_turn,
     "Accelerate": accelerate,
+    "Slow": slow,
+    "Stopped": stopped,
+    "Speed": speed,
+    "Route": route,
+    "Overtake": overtake,
+    "Late Turn": late_turn,
+    "Early Turn": early_turn,
+    "Lane Position": lane_position,
+    "Emergency Services": emergency_services,
+    "Diversion": diversion,
+    "Close Proximity": close_proximity,
+    "Uncategorised": uncategorised,
 }
 
 # Sidebar navigation
---
+++ taxonomy/slow.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import os
+
+def app():
+    st.title("Taxonomy: 'Slow'")
+
+    # Video folder path
+    video_folder = "media/slow"
+
+    # Get the list of folder names in video_folder
+    folder_names = [name for name in os.listdir(video_folder) if os.path.isdir(os.path.join(video_folder, name))]
+
+    # Display a select box for choosing the folder
+    why_selection = st.selectbox(
+        'Please select which "WHY" examples you would like to see:',
+        folder_names
+    )
+    st.markdown("---")
+
+
+    # Construct the directory path based on the selected option
+    video_dir = os.path.join(video_folder, why_selection)
+
+    # Get all video files in the selected directory
+    video_files = os.listdir(video_dir)
+    video_files = [os.path.join(video_dir, f) for f in video_files]
+
+    if video_files:
+        # Display each video in the directory
+        for video_path in video_files:
+            st.video(video_path)
+    else:
+        st.warning("No videos found in the selected directory.")
+
